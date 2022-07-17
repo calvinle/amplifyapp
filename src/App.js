f01813c3faad2dac710d8e1ea7bdd5eb79ec70e3
Calvin Le <calvin.md.le@gmail.com>
2022-07-17 17:22:44 -0500

    Amplify dependencies and homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {ß
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>I am Calvin Leß</h1>
       </header>
     </div>
   );
